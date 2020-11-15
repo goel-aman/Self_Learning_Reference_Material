781aabe386893b8e9559c0a1e2c9bbee1d4e6ace
goel-aman <amangoel9873572693@gmail.com>
2020-11-15 18:12:45 +0530

    new-codes-added

---
+++ Coding Blocks Competitive Programming Course/Recursion/Recursion_Introduction.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if(n == 0){
+        return 1;
+    }
+
+    return fact(n - 1) * n;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<fact(n)<<endl;
+    return 0;
+}
