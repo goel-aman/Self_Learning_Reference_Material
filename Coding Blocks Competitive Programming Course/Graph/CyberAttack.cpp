22059dd0c4cd77d225f71df32f3de3d46181f496
goel-aman <amangoel9873572693@gmail.com>
2020-11-20 22:21:30 +0530

    new-codes-added

--- Coding Blocks Competitive Programming Course/Graph/CyberAttack.cpp
+++ Coding Blocks Competitive Programming Course/Graph/CyberAttack.cpp
@@ -7,20 +7,35 @@ using namespace std;
 
 class graph{
     // node,node,weight
-    unordered_map<int,vector<pair<int,int>>> um;
+    int * rank;
+    int * parent;
+    vector<vector<int>> vector_v;
     int V;
     
     public:
     
     graph(int v){
         this->V = v;
+        rank = new int[V + 1];
+        parent = new int[V + 1];
+    }
+
+    int findParent(int x){
+        if(x == -1){
+            return x;
+        }
+
+        return parent[x] = findParent(x);
     }
 
     void addEdge(int a,int b,int weight){
-        um[a].push_back(make_pair(b,weight));
-        um[b].push_back(make_pair(a,weight));
+        vector_v.pb({weight,a,b});
     }
 
+    void solve()
+    {
+
+    }
     
 };
 
@@ -33,7 +48,9 @@ int main(){
         int sum = 0;
         int a,b,weight;
         cin>>a>>b>>weight;
+        g.addEdge(a,b,weight);
         sum += weight;       
     }
+    g.solve();
     return 0;
 }
