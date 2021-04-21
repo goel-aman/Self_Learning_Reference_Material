4287c827259c554e825d6201b559ec52d31ef15e
goel-aman <amangoel9873572693@gmail.com>
2021-04-21 22:56:55 +0530

    JavaSocketProgrammingCode

--- JavaCodes/echoClient.java
+++ JavaCodes/echoClient.java
@@ -1,11 +1,21 @@
 import java.net.*;
 
+// import jdk.internal.org.jline.utils.InputStreamReader;
+
+import java.io.*;
 public class echoClient {
     public static void main(String[] args) throws Exception
     {
-        
+
         System.out.println("Client Started .....");
         Socket soc = new Socket("localhost",9806);
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a string : ");
+        String str = userInput.readLine();
+        PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+        out.println(str);
         
+        BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        System.out.println(in.readLine());
     }
 }
--- JavaCodes/echoServer.java
+++ JavaCodes/echoServer.java
@@ -1,11 +1,18 @@
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.io.*;
 public class echoServer {
     public static void main(String[] args) throws Exception{
+        
         System.out.println("Waiting for the client .... ");
         ServerSocket ss = new ServerSocket(9806);
         Socket soc = ss.accept();
         System.out.println("Connection Established. ... . . .");
+        BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        String str  = in.readLine();
+        PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+        out.println("Server says : " + str);
+        ss.close();
+       
     }
 }
